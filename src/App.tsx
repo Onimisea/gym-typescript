217a348ff1a0341245e315f48cd308c9e277b31d
Onimisea <onimisea@gmail.com>
2023-04-02 01:19:41 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import Navbar from "@/scenes/navbar";
 
 function App() {
-  return <div className="app bg-gray-20">app</div>;
+  return (
+    <div className="app bg-gray-50">
+      <Navbar /> and App
+    </div>
+  );
 }
 
 export default App;
